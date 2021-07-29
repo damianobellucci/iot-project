adc15018bbdc128e735380977cefc9a054037570
Damiano <belluccidamiano@gmail.com>
2021-07-29 17:30:16 +0200

    major update, added interaction with fluxdb

--- bridge/sub.js
+++ bridge/sub.js
@@ -1,3 +1,17 @@
+////////////////////////////////////////
+const { InfluxDB } = require('@influxdata/influxdb-client')
+
+// You can generate a Token from the "Tokens Tab" in the UI
+const token = 'VDO-0ZVFllIx3vpmto_WEFHkV6pIKmUDGSkhpkB7YCpmRvcSIq6snL-fLw0psiH23_EpWpH0MZ32ymRyfOLfoQ=='
+const org = 'damiano'
+const bucket = 'damiano'
+
+const { Point } = require('@influxdata/influxdb-client')
+
+////////////////////////////////////////
+
+
+
 var mqtt = require('mqtt');
 
 const IPbroker = 'mqtt://130.136.2.70:1883'
@@ -26,8 +40,63 @@ client.on("error", function (error) {
 });
 
 client.on('message', function (topic, message, packet) {
-    console.log("topic: " + topic + " | message: " + message);
-    console.log(packet)
+
+    const client = new InfluxDB({ url: 'http://localhost:8086', token: token })
+
+    const writeApi = client.getWriteApi(org, bucket)
+    writeApi.useDefaultTags({ id: 'host1', gps: "aaaa" })
+
+    let m = message.toString();
+
+    let data = m.split(";");
+
+
+
+    for (let i = 0; i < data.length; i++) {
+        const point = new Point('mem');
+        if (data[i] != undefined) {
+            let info;
+            if (i == 0) {
+                info = 'temperature';
+                point.floatField(info, data[i])
+            }
+            else if (i == 1) {
+                info = 'humidity';
+                point.floatField(info, data[i])
+            }
+            else if (i == 2) {
+                info = 'soil_mosture';
+                point.floatField(info, data[i])
+            }
+            else if (i == 3) {
+                info = 'GPS';
+                point.floatField(info, data[i].toString())
+            }
+            else if (i == 4) {
+                info = 'rssi';
+                point.floatField(info, data[i])
+            }
+
+
+            writeApi.writePoint(point)
+            writeApi
+                .close()
+                .then(() => {
+                    console.log('FINISHED')
+                })
+                .catch(e => {
+                    console.error(e)
+                    console.log('\\nFinished ERROR')
+                })
+        }
+
+
+
+        console.log("topic: " + topic + " | message: " + message);
+        //console.log(packet)
+    }
 });
 
 client.subscribe(topic_1, options)
+
+
